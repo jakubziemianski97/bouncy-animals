21d77f8121742f4bbfba014490240263a5bc7825
Jakub Ziemiański <jakubziemianski@air-jakub.home>
2024-01-06 22:12:47 +0100

    added functions, backgrounds and styles

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ window.onload = () => {
 
 const animal = document.querySelector('.animal')
 const gameField = document.querySelector('.game-field')
+const pipesDiv = document.querySelector('.pipes')
 const pipeBottom = document.querySelector('.bottom')
 const pipeTop = document.querySelector('.top')
 const scoreElement = document.querySelector('.score')
@@ -15,11 +16,14 @@ class Game {
 
 	pipes = []
 	pipeTopImg = getComputedStyle(pipeTop).backgroundImage
+
 	pipeBottomImg = getComputedStyle(pipeBottom).backgroundImage
-	pipeTopY = 0
-	pipeTopX = 0
-	pipeBottomY = 0
-	pipeBottomX = 0
+	// pipeTopY = 0
+	// pipeTopX = 0
+	// pipeBottomY = 0
+	// pipeBottomX = 0
+	animalHeight = 43
+	animalWidth = 38
 	pipeWidth = 47
 	pipeHeight = 382
 	pipesGap = 120
@@ -28,9 +32,6 @@ class Game {
 	gameFieldHeight = gameField.offsetHeight
 
 	init = () => {
-		// this.canvas = document.getElementById('canvas')
-		// this.context = this.canvas.getContext('2d')
-
 		document.addEventListener('click', this.moveUp)
 		document.addEventListener('keydown', e => {
 			if (e.key == ' ') {
@@ -42,14 +43,15 @@ class Game {
 
 	startGame = () => {
 		const fps = 60
-		setInterval(this.updateGame, 1000 / fps)
+		// setInterval(this.updateGame, 1000 / fps)
+		// setInterval(this.updateGame, 1)
 
 		this.addPipe()
 	}
 
 	addPipe = () => {
 		let x = this.gameFieldWidth
-		let y = Math.floor(Math.random() * this.pipeWidth) - this.pipeHeight
+		let y = Math.floor(Math.random() * this.pipeHeight) - this.pipeHeight
 
 		this.pipes.push({
 			top: {
@@ -57,40 +59,125 @@ class Game {
 				y: y,
 				width: this.pipeWidth,
 				height: this.pipeHeight,
+				img: this.pipeTopImg,
 			},
 			bottom: {
 				x: x,
 				y: y + this.pipeHeight + this.pipesGap,
 				width: this.pipeWidth,
 				height: this.pipeHeight,
+				img: this.pipeBottomImg,
 			},
 		})
 	}
+	// createNewPipeTop = () => {
+	// 	const divPipeTop = document.createElement('div')
+	// 	divPipeTop.classList.add('top')
+
+	// 	pipesDiv.appendChild(divPipeTop)
+	// }
 
 	updateGame = () => {
-		// Logika gry
 		this.addGravity()
+		this.checkCollision()
 		this.render()
+
+		// console.log(game.pipes)
 	}
 
 	addGravity = () => {
 		this.posY += this.gravity
 	}
 
-	render = () => {
-		// this.drawPipes()
-		animal.style.top = this.posY + 'px'
-		pipeTop.style.top = this.pipeTopY + 'px'
-		pipeTop.style.right = this.pipeTopX + 'px'
-		pipeBottom.style.bottom = this.pipeBottomY + 'px'
-		pipeBottom.style.right = this.pipeBottomX + 'px'
-	}
-	// drawPipes = () => {
-	// 	const pipesToDraw = [...this.pipes]
-	// 	drawPipe(pipeTopImg, pipeTopX, pipeTopY)
+	checkCollision = () => {
+		if (this.posY > this.gameFieldHeight - this.animalHeight) {
+			this.moveUp()
+		}
 
-	// 	this.pipeTopX--
-	// }
+		if (this.posY < 0) {
+			this.posY = 0
+		}
+
+		const pipesToCheck = [...this.pipes]
+		const animalX = this.posX
+		const animalY = this.posY
+		const animalW = this.animalHeight
+		const animalH = this.animalWidth
+
+		pipesToCheck.forEach(pipe => {
+			if (animalX + animalW > pipe.top.x && animalX <= pipe.top.x + pipe.top.width) {
+				if (animalY < pipe.top.y + pipe.top.height || animalY + animalH > pipe.bottom.y) {
+					this.restart()
+				}
+			}
+			if (pipe.top.x == -1) {
+				this.score++
+				scoreElement.textContent = `${this.score}`
+				if (this.score % 2 == 0) {
+					this.pipesGap--
+
+					if (this.pipesGap < 100) {
+						this.pipesGap == 100
+					}
+				}
+			}
+		})
+	}
+
+	restart = () => {
+		this.posX = 30
+		this.posY = 240
+		this.score = 0
+		scoreElement.textContent = '0'
+
+		this.pipes = []
+		this.pipesGap = 120
+
+		this.addPipe()
+	}
+
+	render = () => {
+		this.drawPipes()
+		animal.style.top = this.posY + 'px'
+	}
+	drawPipes = () => {
+		pipesDiv.innerHTML = ''
+
+		const pipesToDraw = [...this.pipes]
+
+		pipesToDraw.forEach(pipe => {
+			const createNewPipeTop = document.createElement('div')
+			const createNewPipeBottom = document.createElement('div')
+
+			createNewPipeTop.classList.add('top')
+			createNewPipeBottom.classList.add('bottom')
+
+			createNewPipeTop.style.top = pipe.top.y + 'px'
+			createNewPipeTop.style.height = pipe.top.height + 'px'
+			createNewPipeTop.style.backgroundImage = getComputedStyle(pipeTop).backgroundImage
+
+			createNewPipeBottom.style.top = pipe.bottom.y + 'px'
+			createNewPipeBottom.style.height = pipe.bottom.height + 'px'
+			createNewPipeBottom.style.backgroundImage = getComputedStyle(pipeBottom).backgroundImage
+
+			pipeTop.style.left = --pipe.top.x + 'px'
+			pipeBottom.style.left = --pipe.bottom.x + 'px'
+
+			createNewPipeTop.style.left = pipe.top.x + 'px'
+			createNewPipeBottom.style.left = pipe.bottom.x + 'px'
+
+			pipesDiv.appendChild(createNewPipeTop)
+			pipesDiv.appendChild(createNewPipeBottom)
+
+			if (pipe.top.x + this.pipeWidth < -100) {
+				this.pipes.shift()
+			}
+
+			if (pipe.top.x === 150) {
+				this.addPipe()
+			}
+		})
+	}
 
 	moveUp = () => {
 		this.posY -= 30
